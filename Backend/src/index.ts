709e1912b554aa0ce81ad3079ce289880255def3
Teaghan <eteaghan@gmail.com>
2026-05-24 15:55:34 +1200

    Chore: Test Db connection

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,23 @@
 // index.js
+import { query } from "#config/neon-connection.js";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT ?? "3001";
 
 
-app.get("/", (req, res) => {
+app.get("/promise-chain", (req, res) => {
+  query("SELECT * FROM USERS")
+  .then((users)=>{
+    console.log(users);
+  })
+
   res.send("Hello World!");
   console.log("Response sent");
+});
+app.get("/async", async (req, res) => {
+ const users = await query("SELECT * FROM USERS");
+ console.log(users)
 });
 
 app.listen(port, () => {
