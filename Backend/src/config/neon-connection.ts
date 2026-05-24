0691df253d2211204d68c0fcecccc1a55ed2f791
Teaghan <eteaghan@gmail.com>
2026-05-24 15:39:20 +1200

    Feature : DB Connection
    - Added neon db connection string and pool for DB connection

---
+++ Backend/src/config/neon-connection.ts
@@ -0,0 +1,13 @@
+import { Pool, QueryResultRow } from 'pg'
+
+
+export const pool = new Pool({
+    connectionString: process.env.DB_CONNECTION_STRING,
+})
+
+pool.on('error', () => console.error("Error When Connecting to NEON database"));
+
+export const query =  async <T extends QueryResultRow> (text: string, values ?: unknown[]) : Promise<T[]>=>{
+   const {rows} = await  pool.query<T>(text,values);
+   return rows;
+}
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,9 @@
 // index.js
 import express from "express";
+
 const app = express();
 const port = process.env.PORT ?? "3001";
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
